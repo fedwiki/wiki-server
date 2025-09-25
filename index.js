e85c819a4623b4a7ec8049d6ebcd6e2f2e1b31b3
Paul Rodwell <paul.rodwell@btinternet.com>
2025-09-25 11:45:57 +0100

    Update import statements to modern ESM syntax
    remove unused imports

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 // Simple file so that if you require this directory
 // in node it instead requires ./lib/server.coffee
 // with coffee-script already loaded.
-import('coffeescript')
-import('coffeescript/register.js')
+// import('coffeescript')
+// import('coffeescript/register.js')
 
-const { default: server } = await import('./lib/server.js')
+// const { default: server } = await import('./lib/server.js')
+// export default server
+
+import { default as server } from './lib/server.js'
 export default server
